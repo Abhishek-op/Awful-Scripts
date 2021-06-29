64e2e66c20409115bac26b4c201d2b1c1b62441a
Abhishek-op <83164668+Abhishek-op@users.noreply.github.com>
2021-06-29 18:58:06 +0530

    Update timer.py

--- python/Countdown_clock_and_Timer/timer.py
+++ python/Countdown_clock_and_Timer/timer.py
@@ -8,28 +8,16 @@ times = time.ctime()
 print("\nCurrent Time: ", times)
 
 print(
-    "\n     Welcome to CountdownTimer!\n\n     Let's set up the countdown timer...\n"
+    "\n     Welcome to Awfulscripts\n"
 )
-
-# User input for the timer
-hours = int(input("     How many hours? "))
-minutes = int(input("     How many minutes? "))
-seconds = int(input("     How many seconds? "))
-
-# To display message when the given value is not a number
-if hours or minutes or seconds == "":
-    print("\n Invalid entry. You must enter a number.")
-
-# Conversion of hours amd minutes into seconds
+hours = int(input("Enter hours? "))
+minutes = int(input("Enter minutes? "))
+seconds = int(input("Enter seconds? "))
 hrsToSec = (hours * 60) * 60
 mnsToSec = (minutes * 60)
 seconds = seconds
-
 seconds = hrsToSec + mnsToSec + seconds
 print("\n Timer has been set for " + str(seconds) + " seconds.")
-
-# Loop for displaying the timer
-
 for i in range(seconds, -1, -1):
     displayHours = int(seconds / 3600)
     displayMinutes = int(seconds / 60)
@@ -43,6 +31,6 @@ for i in range(seconds, -1, -1):
         str(displayMinutes).zfill(2),
         str(displaySeconds).zfill(2)))
     seconds -= 1
-    time.sleep(1)  # delays in the excution of a program for 1 second
+    time.sleep(1)
 
-print("\n Time is over.")
+print("\n Time is ended.")
